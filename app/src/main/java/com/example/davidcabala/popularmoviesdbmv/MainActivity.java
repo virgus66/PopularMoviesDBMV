d477bb785ce583ea0902473cddcea95211199351
David <virgus66@users.noreply.github.com>
2018-12-17 15:28:12 +0000

    working biuld

--- app/src/main/java/com/example/davidcabala/popularmoviesdbmv/MainActivity.java
+++ app/src/main/java/com/example/davidcabala/popularmoviesdbmv/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        new FetchMovieAsync("6b6d667ed3e438074ddf460ffe25a8ca").execute();
     }
 }
